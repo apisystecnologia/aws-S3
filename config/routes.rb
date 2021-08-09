2147b00d424069d53aeacd425bbae5e7553fcd85
GabrieleRochaB <gabriele.rocha@apisys.com.br>
2021-08-09 10:13:12 -0300

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'file_uploads/new'
+  resources :buckets
+  root to: "buckets#index"
+  get 'file_uploads/new'
+  resources :buckets do
+    resources :file_uploads, only: [:new, :create, :destroy]
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
